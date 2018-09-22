b4c28dd8a53dc5f4524d49e18ca27dbbbd731186
Rafael Sant' Anna <rafaeel.16@gmail.com>
2018-09-22 09:32:34 -0300

    setup inicial aula 5

---
+++ javascript/aula 05/js/calcula-imc.js
@@ -0,0 +1,62 @@
+// Instanciando os pacientes da tabela
+var pacientes = document.querySelectorAll(".paciente");
+
+// Loop que percorre os pacientes da tabela e calcula o IMC
+for (var i = 0; i < pacientes.length; i = i + 1) {
+
+    //variavel referente ao indice do paciente
+    var paciente = pacientes[i];
+
+    // variaveis elemento td
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var tdImc = paciente.querySelector(".info-imc");
+
+    //Variaveis dados da tabela
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+
+    // Variaveis de validação
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValido = validaAltura(altura);
+
+    // Validação altura e peso
+    if (!pesoEhValido) {
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido";
+        paciente.classList.add('paciente-invalido');
+    }
+
+    if (!alturaEhValido) {
+        alturaEhValido = false;
+        tdImc.textContent = "Altura Inválido";
+        paciente.classList.add('paciente-invalido');
+    }
+
+    // Calculo IMC
+    if (pesoEhValido && alturaEhValido) {
+        tdImc.textContent = calculaImc(peso, altura);
+    }
+}
+
+function validaPeso(peso) {
+    if (peso >= 0 && peso <= 200 ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validaAltura(altura) {
+    if (altura >= 0 && altura <= 3.00) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function calculaImc(peso, altura) {
+    var imc = 0;
+    imc = peso / (altura * altura);
+    return imc.toFixed(2);
+}
